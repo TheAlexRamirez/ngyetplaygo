b24b23e404781fd252f925b3fe1b0a5c3b872c49
Alex Ramirez <alexramirez@Alexs-MacBook-Pro.local>
2017-03-17 11:05:47 -0600

    Login reactive form created
    Reactive forms module added to app module

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,35 @@
 // login.component.ts
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
 
 import { UserService } from '../user.service';
+
 
 @Component({
   templateUrl: 'login.component.html',
   styleUrls : ['login.component.css']
 })
 export class LoginComponent {
-  constructor(private userService: UserService, private router: Router) {}
+  constructor(private userService: UserService, private router: Router,public fb: FormBuilder) {}
+
+  public loginForm = this.fb.group({
+    email: ["", Validators.required],
+    password: ["", Validators.required]
+  });
+
+  doLogin(event){
+    // Grab the values of the controls:
+    let email = this.loginForm.get('email').value;
+    let password = this.loginForm.get('password').value;
+
+    this.onSubmit(email,password);
+
+  }
 
   onSubmit(email, password) {
+    console.log(this.loginForm.value);
+
     this.userService.login(email, password).subscribe((result) => {
       if (result) {
         this.router.navigate(['']);
