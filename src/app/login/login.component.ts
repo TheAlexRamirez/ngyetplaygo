7f9519db338ac08ecd5027fe81c868f8fb0b3a9c
Alex Ramirez <alexramirez@Alexs-MacBook-Pro.local>
2017-03-17 10:35:58 -0600

    Login component structure defined

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,6 +6,7 @@ import { UserService } from '../user.service';
 
 @Component({
   templateUrl: 'login.component.html',
+  styleUrls : ['login.component.css']
 })
 export class LoginComponent {
   constructor(private userService: UserService, private router: Router) {}
